7e559ebad1c80a17d70278fa4b7fb561c6b9c042
Jean-Marie <jm@jmporchet.ch>
2019-05-16 17:46:54 +0200

    feat: connect to sensor and display temperature

--- App.js
+++ App.js
@@ -1,109 +1,92 @@
+import {Buffer} from 'buffer';
 import React, { Component } from 'react';
 import { Platform, View, Text } from 'react-native';
 import { BleManager } from 'react-native-ble-plx';
 
+const deviceId = "337C0420-AB8F-2965-E66A-37DDDC6EB2C0";
+const tempServiceId = "F000AA00-0451-4000-B000-000000000000";
+const writeChar = "F000AA02-0451-4000-B000-000000000000";
+const readChar = "F000AA01-0451-4000-B000-000000000000";
+
+
 export default class SensorsComponent extends Component {
 
   constructor() {
-    super()
-    this.manager = new BleManager()
-    this.state = {info: "", values: {}, uuidList: [], device: null}
-    this.sensors = []
+    super();
+    this.manager = new BleManager();
+    this.state = {info: ""};
   }
 
   info(message) {
-    this.setState({info: message})
+    this.setState({info: message});
   }
 
   error(message) {
-    this.setState({info: "ERROR: " + message})
-  }
-
-  updateValue(key, value) {
-    this.setState({values: {...this.state.values, [key]: value}})
+    this.setState({ info: "ERROR: " + message });
   }
 
   componentWillMount() {
     if (Platform.OS === 'ios') {
       this.manager.onStateChange((state) => {
-        if (state === 'PoweredOn') this.scanAndConnect()
+        if (state === 'PoweredOn') this.scanAndConnect();
       })
     } else {
-      this.scanAndConnect()
-    }
-  }
-
-  listenerFunction = async (error, device) => {
-    this.info("Scanning...")
-    // if (device.name && device.name.indexOf('MacBook Pro') === -1) {
-      console.log(device)
-    // }
-
-    if (error) {
-      this.error(error.message)
-      return
-    }
-
-    if (device.localName === 'CC2650 SensorTag') {
-      this.info("CC2650 SensorTag")
-      this.setState({device});
-      console.log('Connecting to CC2650 SensorTag...');
-      this.manager.stopDeviceScan()
-      console.log('stopDeviceScan: ');
-      const deviceInfo = await device.connect();
-      console.log('device.connect: ', deviceInfo);
-      await device.discoverAllServicesAndCharacteristics();
-      const services = await device.services()
-      console.log('device.services: ', services);
-      const characteristics = await this.manager.characteristicsForDevice("337C0420-AB8F-2965-E66A-37DDDC6EB2C0", "0000180f-0000-1000-8000-00805f9b34fb")
-      console.log('characteristics: ', characteristics);
-      services.map(service => this.setState(prev => ({uuidList: [...prev.uuidList, service] })));
-      // for (i = 8; i < services.length; i++) {
-      //   console.log('service', i);
-      //   console.log('device.id: ', device.id);
-      //   console.log('services[i].uuid: ', services[i].uuid);
-      //   console.log(await this.manager.characteristicsForDevice(device.id, services[i].uuid));
-      // }
-      services.map(async (service) => console.log('device.characteristics: ', await this.manager.characteristicsForDevice(device.id, service.uuid)))
-      // this.setupNotifications(device)
-      console.log('notifs');
+      this.scanAndConnect();
     }
   }
 
   scanAndConnect() {
-    this.manager.startDeviceScan(null, null, this.listenerFunction);
-  }
+    this.manager.startDeviceScan(null, null, async (error, device) => {
+      this.info("Scanning...");
+  
+      if (error) {
+        this.error(error.message);
+        return
+      }
+  
+      if (device.localName === 'CC2650 SensorTag') {
+        this.info("CC2650 SensorTag");
+        this.setState({ device });
+        this.info('Connecting to CC2650 SensorTag...');
 
-  async setupNotifications(device) {
-    
-    for (const id in this.state.uuidList) {
-      const service = this.serviceUUID(id)
-      const characteristicW = this.writeUUID(id)
-      const characteristicN = this.notifyUUID(id)
+        this.manager.stopDeviceScan();
+        this.info('stopping Device scan');
 
-      const characteristic = await device.writeCharacteristicWithResponseForService(
-        service, characteristicW, "AQ==" /* 0x01 in hex */
-      )
-
-      device.monitorCharacteristicForService(service, characteristicN, (error, characteristic) => {
-        if (error) {
-          this.error(error.message)
-          return
-        }
-        this.updateValue(characteristic.uuid, characteristic.value)
-      })
-    }
+        const deviceInfo = await device.connect();
+        this.info('the SensorTag is connected');
+        console.log('deviceInfo: ', deviceInfo);
+        
+        // this is mandatory even if we already know the service/char UUIDs
+        await device.discoverAllServicesAndCharacteristics();
+        const services = await device.services();
+        this.info('services have been discovered');
+        console.log('services: ', services);
+        
+        const characteristics = await this.manager.characteristicsForDevice(deviceId, tempServiceId);
+        this.info('characteristics for the temperature service have been discovered');
+        console.log('characteristics for the temperature service: ', characteristics);
+        
+        const characteristic = await device.writeCharacteristicWithResponseForService(tempServiceId, writeChar, "AQ==" /* 0x01 in hex */);
+        this.info('R/W mode enabled');
+        console.log('characteristic: ', characteristic);
+        
+        this.manager.monitorCharacteristicForDevice(deviceId, tempServiceId, readChar, (err, characteristic) => {
+          if (err) { 
+            console.log('error', err);
+            return;
+          }
+          // the data format is stored as 16 bit unsigned integer, we need to convert it
+          const currentTemp = Buffer.from(characteristic.value, 'base64').readUInt16LE(0) / 128;
+          this.info('current temperature ' + currentTemp);
+        })
+      }
+    });
   }
 
   render() {
     return (
-      <View style={{marginTop: 50}}>
+      <View style={{margin: 50}}>
         <Text>{this.state.info}</Text>
-        {Object.keys(this.sensors).map((key) => {
-          return <Text key={key}>
-                   {this.sensors[key] + ": " + (this.state.values[this.notifyUUID(key)] || "-")}
-                 </Text>
-        })}
       </View>
     )
   }
