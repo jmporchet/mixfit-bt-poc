756950c7604ae9b725ccf84e8a9eaefa541d9f15
Jean-Marie <jm@jmporchet.ch>
2019-05-16 17:44:40 +0200

    chore: setup expokit project

---
+++ ios/bluetooth.swift
@@ -0,0 +1,9 @@
+//
+//  bluetooth.swift
+//  mixfit-bt-poc
+//
+//  Created by Jean-Marie Porchet on 14.05.19.
+//  Copyright © 2019 650 Industries, Inc. All rights reserved.
+//
+
+import Foundation
