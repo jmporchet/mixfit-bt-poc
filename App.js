fde57e7fdd80ba2316d56f744edddee85975251a
Jean-Marie <jm@jmporchet.ch>
2019-05-20 12:42:48 +0200

    feat: disconnect from device at the end

--- App.js
+++ App.js
@@ -70,15 +70,8 @@ export default class SensorsComponent extends Component {
         this.info('R/W mode enabled');
         console.log('characteristic: ', characteristic);
         
-        this.manager.monitorCharacteristicForDevice(deviceId, tempServiceId, readChar, (err, characteristic) => {
-          if (err) { 
-            console.log('error', err);
-            return;
-          }
-          // the data format is stored as 16 bit unsigned integer, we need to convert it
-          const currentTemp = Buffer.from(characteristic.value, 'base64').readUInt16LE(0) / 128;
-          this.info('current temperature ' + currentTemp);
-        })
+        await this.manager.cancelDeviceConnection(device.id).catch(err => { console.log('caught', err)});
+        this.info('Disconnected successfully from Mixfit One');
       }
     });
   }
