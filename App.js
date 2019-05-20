0cc89edef20395b57f4f95bf9cddd240c80426de
Jean-Marie <jm@jmporchet.ch>
2019-05-20 12:43:40 +0200

    refactor: customize for Mixfit One

--- App.js
+++ App.js
@@ -8,6 +8,11 @@ const tempServiceId = "F000AA00-0451-4000-B000-000000000000";
 const writeChar = "F000AA02-0451-4000-B000-000000000000";
 const readChar = "F000AA01-0451-4000-B000-000000000000";
 
+const drinkServiceUUID = '125A7D72-9012-421A-AF95-BD6E53B7DD67';
+const makeDrinkCharUUID = '125A7D73-9012-421A-AF95-BD6E53B7DD67';
+const deviceStatusServiceUUID = '10041AE3-17A2-4025-8DB0-00F6AD986120';
+const deviceStatusCharUUID = '10041AE4-17A2-4025-8DB0-00F6AD986120';
+const recipeCompletionPercentageCharUUID = '10041AE5-17A2-4025-8DB0-00F6AD986120';
 
 export default class SensorsComponent extends Component {
 
@@ -37,23 +42,24 @@ export default class SensorsComponent extends Component {
 
   scanAndConnect() {
     this.manager.startDeviceScan(null, null, async (error, device) => {
-      this.info("Scanning...");
+      this.info("Scanning device...");
+      console.log('Scanning device: ', device. name, device.localName);
   
       if (error) {
         this.error(error.message);
         return
       }
   
-      if (device.localName === 'CC2650 SensorTag') {
-        this.info("CC2650 SensorTag");
+      if (device.localName === 'Mixfit One') {
+        this.info("Mixfit One");
         this.setState({ device });
-        this.info('Connecting to CC2650 SensorTag...');
+        this.info('Connecting to Mixfit One...');
 
         this.manager.stopDeviceScan();
         this.info('stopping Device scan');
 
         const deviceInfo = await device.connect();
-        this.info('the SensorTag is connected');
+        this.info('the Mixfit One is connected');
         console.log('deviceInfo: ', deviceInfo);
         
         // this is mandatory even if we already know the service/char UUIDs
@@ -62,12 +68,12 @@ export default class SensorsComponent extends Component {
         this.info('services have been discovered');
         console.log('services: ', services);
         
-        const characteristics = await this.manager.characteristicsForDevice(deviceId, tempServiceId);
-        this.info('characteristics for the temperature service have been discovered');
-        console.log('characteristics for the temperature service: ', characteristics);
+        const characteristics = await this.manager.characteristicsForDevice(device.id, drinkServiceUUID);
+        this.info('characteristics for the drink service have been discovered');
+        console.log('characteristics for the drink service: ', characteristics);
         
-        const characteristic = await device.writeCharacteristicWithResponseForService(tempServiceId, writeChar, "AQ==" /* 0x01 in hex */);
-        this.info('R/W mode enabled');
+        const characteristic = await device.writeCharacteristicWithResponseForService(drinkServiceUUID, makeDrinkCharUUID, "AQ==" /* 0x01 in hex */);
+        this.info('Sent drink request');
         console.log('characteristic: ', characteristic);
         
         await this.manager.cancelDeviceConnection(device.id).catch(err => { console.log('caught', err)});
